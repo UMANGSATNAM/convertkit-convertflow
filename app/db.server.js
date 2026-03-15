4329827bffbc0d8cbbabbf11185dbb8ddd4e120b
Umangpatel11 <umangptl11@gmail.com>
2026-03-15 13:39:50 +0530

    fix(prisma): migrate to 7.5.0 and inject datasourceUrl dynamically

--- app/db.server.js
+++ app/db.server.js
@@ -1,11 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
+const prismaOptions = {
+  datasourceUrl: process.env.DATABASE_URL
+};
+
 if (process.env.NODE_ENV !== "production") {
   if (!global.prismaGlobal) {
-    global.prismaGlobal = new PrismaClient();
+    global.prismaGlobal = new PrismaClient(prismaOptions);
   }
 }
 
-const prisma = global.prismaGlobal ?? new PrismaClient();
+const prisma = global.prismaGlobal ?? new PrismaClient(prismaOptions);
 
 export default prisma;
