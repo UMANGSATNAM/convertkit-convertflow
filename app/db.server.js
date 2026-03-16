ef12739a0dac102ca0e6f0581c2d7ab2a384a286
Umangpatel11 <umangptl11@gmail.com>
2026-03-17 02:19:40 +0530

    fix(db): revert PrismaClient to empty constructor to resolve Unknown property validation error

--- app/db.server.js
+++ app/db.server.js
@@ -2,19 +2,13 @@ import { PrismaClient } from "@prisma/client";
 
 let prisma;
 
-const url = process.env.DATABASE_URL || process.env.MYSQL_URL || "";
-
 if (process.env.NODE_ENV !== "production") {
   if (!global.__prisma) {
-    global.__prisma = new PrismaClient({
-      datasources: { db: { url } },
-    });
+    global.__prisma = new PrismaClient();
   }
   prisma = global.__prisma;
 } else {
-  prisma = new PrismaClient({
-    datasources: { db: { url } },
-  });
+  prisma = new PrismaClient();
 }
 
 export default prisma;
