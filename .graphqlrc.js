29c745c6bc9b5ab4058fb1ecf7d1b83e01b38cee
OmniBuilder <engineering@omnibuilder.com>
2026-05-23 01:39:18 +0530

    Update apiVersion to October25 to support themeDuplicate mutation

--- .graphqlrc.js
+++ .graphqlrc.js
@@ -6,7 +6,7 @@ function getConfig() {
     projects: {
       default: shopifyApiProject({
         apiType: ApiType.Admin,
-        apiVersion: ApiVersion.July25,
+        apiVersion: ApiVersion.October25,
         documents: [
           "./app/**/*.{js,ts,jsx,tsx}",
           "./app/.server/**/*.{js,ts,jsx,tsx}",
